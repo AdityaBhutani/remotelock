064cd5816ed173f28affa2b944bab70fec95789c
Aditya Bhutani <adityab@Aditya-ka-MacBook-Pro.local>
2022-03-11 06:26:16 +0530

    Rows Generator for both kind of files

--- app/people_controller.rb
+++ app/people_controller.rb
@@ -1,12 +1,16 @@
+# frozen_string_literal: true
+
+require_relative 'rows_generator'
+require_relative 'rows_normalizer'
+require_relative 'rows_sorter'
 class PeopleController
+  attr_reader :params
+
   def initialize(params)
     @params = params
   end
 
   def normalize
+    RowsGenerator.generate(params[:dollar_format], '$')
   end
-
-  private
-
-  attr_reader :params
 end
